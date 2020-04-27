58f0de294d66747d57d5a8898e8c4f72bdb04e63
황상일 <si8363@soongsil.ac.kr>
2020-04-28 00:39:06 +0900

    # TTS 호출 함수 작성 완료 - 사용법 ```java         private TTS tts;        //인스턴스 변수로 선언         tts = new TTS(getApplicationContext());         tts.speech(msg);         tts.shutdownTTS();       // 기능 종료시 ```

--- app/src/main/java/com/cobitsa/jarvis/MainActivity.java
+++ app/src/main/java/com/cobitsa/jarvis/MainActivity.java
@@ -1,14 +1,34 @@
 package com.cobitsa.jarvis;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.cobitsa.jarvis.com.cobitsa.jarvis.voice.TTS;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
+    // TTS 호출 예제
+    private TTS tts;
+    private Button btnEnter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        tts = new TTS(getApplicationContext());
         setContentView(R.layout.activity_main);
+        btnEnter = (Button) findViewById(R.id.button);
+        btnEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tts.speech("안녕하세요?");
+            }
+        });
+    }
+    @Override
+    protected void onDestroy() {
+        tts.shutdownTTS();
+        super.onDestroy();
     }
 }
