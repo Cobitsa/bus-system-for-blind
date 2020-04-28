5906513c9d137c9778758adde33d0d2e065da3cd
황상일 <si8363@soongsil.ac.kr>
2020-04-28 23:09:53 +0900

    # STT 호출 함수 작성 완료 - 사용법 ```java         private Activity mainActivity = this;  //메인 엑티비티 변수 생성         privateSTT stt;        //인스턴스 변수로 선언         stt = new STT(getApplicationContext());         stt.getCommand(mainActivity);         tts.shutdownSTT();       // 기능 종료시 ```

--- app/src/main/java/com/cobitsa/jarvis/MainActivity.java
+++ app/src/main/java/com/cobitsa/jarvis/MainActivity.java
@@ -1,14 +1,37 @@
 package com.cobitsa.jarvis;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.cobitsa.jarvis.com.cobitsa.jarvis.voice.STT;
 
 public class MainActivity extends AppCompatActivity {
+    private Activity mainActivity = this;
+    private STT stt;
+    private Button sttButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        stt = new STT(getApplicationContext());
+        sttButton = (Button) findViewById(R.id.STTButton);
+        sttButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stt.getCommand(mainActivity);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        stt.shutdownSTT();
+        super.onDestroy();
     }
 }
---
+++ app/src/main/java/com/cobitsa/jarvis/com/cobitsa/jarvis/voice/STT.java
@@ -0,0 +1,115 @@
+package com.cobitsa.jarvis.com.cobitsa.jarvis.voice;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.widget.Toast;
+
+import androidx.core.content.ContextCompat;
+
+import java.util.ArrayList;
+
+import static androidx.core.app.ActivityCompat.requestPermissions;
+
+public class STT {
+    Intent intent;
+    SpeechRecognizer mRecognizer;
+    final Context context;
+    ArrayList<String> result = new ArrayList<String>();
+
+    public STT(final Context context) {
+        this.context = context;
+        intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, context.getPackageName());
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ko-KR");
+        mRecognizer = SpeechRecognizer.createSpeechRecognizer(context);
+        mRecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+                Toast.makeText(context, "지금부터 말을 해주세요!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onRmsChanged(float v) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] bytes) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int i) {
+
+            }
+
+            @Override
+            public void onResults(Bundle bundle) {
+                String key = "";
+                key = SpeechRecognizer.RESULTS_RECOGNITION;
+                result = bundle.getStringArrayList(key);
+                String[] rs = new String[result.size()];
+                result.toArray(rs);
+                Toast.makeText(context, rs[0], Toast.LENGTH_SHORT).show();
+
+                System.out.println("=================================================");
+                for (int i = 0; i < result.size(); i++)
+                    System.out.println(result.get(i));
+                System.out.println("=================================================");
+                //여기서부터 형태소 분석 실행 예정
+
+
+                //여기서부터 구문에 맞게 메소드 실행 예정
+            }
+
+            @Override
+            public void onPartialResults(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onEvent(int i, Bundle bundle) {
+
+            }
+        });
+    }
+
+    public void getCommand(Activity activity) {
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(activity, new String[]{Manifest.permission.RECORD_AUDIO}, 1);
+            //권한을 허용하지 않는 경우
+        } else {
+            //권한을 허용한 경우
+            try {
+                mRecognizer.startListening(intent);
+            } catch (SecurityException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void shutdownSTT() {
+        if (mRecognizer != null) {
+            mRecognizer.destroy();
+            mRecognizer.cancel();
+            mRecognizer = null;
+        }
+    }
+}
