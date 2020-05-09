85145a96919c70ddcb999caad58f813564f2993c
황상일 <si8363@soongsil.ac.kr>
2020-05-09 19:37:58 +0900

    스플래시 추가

---
+++ app/src/main/java/com/cobitsa/jarvis/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.cobitsa.jarvis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Start home activity
+        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        // close splash activity
+        finish();
+    }
+}
