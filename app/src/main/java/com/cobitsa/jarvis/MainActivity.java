1357850317577cafc55be18c81684390c96cb839
YoonSoo <fhkaldh@naver.com>
2020-05-11 04:52:39 +0900

    TraceBus 클래스

--- app/src/main/java/com/cobitsa/jarvis/MainActivity.java
+++ app/src/main/java/com/cobitsa/jarvis/MainActivity.java
@@ -2,29 +2,44 @@ package com.cobitsa.jarvis;
 
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.cobitsa.jarvis.com.cobitsa.jarvis.voice.STT;
+import com.cobitsa.jarvis.com.cobitsa.jarvis.bus.common.TraceBus;
 
 public class MainActivity extends AppCompatActivity {
     private Activity mainActivity = this;
     private STT stt;
     private Button sttButton;
+    private static Context context;
+
+    String key = "";
+    String stId = "";
+    String busRouteId = "";
+    String vehId = "";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        MainActivity.context = getApplicationContext();
         setContentView(R.layout.activity_main);
-        stt = new STT(getApplicationContext());
+        stt = new STT();
         sttButton = (Button) findViewById(R.id.STTButton);
         sttButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                stt.getCommand(mainActivity);
+                TraceBus traceBus = new TraceBus(key, stId, busRouteId, vehId);
+                if(traceBus.tracing()) {
+                    Toast.makeText(context, "Test Text", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
@@ -34,5 +49,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         stt.shutdownSTT();
         super.onDestroy();
+    }
+
+    public static Context getAppContext(){
+        return MainActivity.context;
     }
 }
