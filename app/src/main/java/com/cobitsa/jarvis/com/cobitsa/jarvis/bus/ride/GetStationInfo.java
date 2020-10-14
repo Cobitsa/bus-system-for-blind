b4d2f80f101615f7052aa63fb5cdade5e89b4961
YoonSoo <fhkaldh@naver.com>
2020-10-15 02:29:52 +0900

    여러개의 정류장이 검색되었을때 거리 계산해서 가장 가까운 정류장 찾는 로직 추가

--- app/src/main/java/com/cobitsa/jarvis/com/cobitsa/jarvis/bus/ride/GetStationInfo.java
+++ app/src/main/java/com/cobitsa/jarvis/com/cobitsa/jarvis/bus/ride/GetStationInfo.java
@@ -62,9 +62,23 @@ public class GetStationInfo {
             } else if (parsingXML.getLength() == 0)
                 return null;
             else {
-                int nextRadius = Integer.parseInt(radius) / 2;
-                stInfo = getApiData(tmX, tmY, Integer.toString(nextRadius));
-                return stInfo;
+                // 기존 Logic
+//                int nextRadius = Integer.parseInt(radius) / 2;
+//                stInfo = getApiData(tmX, tmY, Integer.toString(nextRadius));
+//                return stInfo;
+
+                // 새로운 Logic
+                ArrayList<ArrayList<Double>> stList = new ArrayList<>();
+                for(int i=0; i<parsingXML.getLength(); i++) {
+                    ArrayList<Double> tmpUnit = new ArrayList<>();
+                    tmpUnit.add(Double.parseDouble(parsingXML.parsing("gpsX", i)));
+                    tmpUnit.add(Double.parseDouble(parsingXML.parsing("gpsY", i)));
+                    stList.add(tmpUnit);
+                }
+                int index = findNearStation(stList, Double.parseDouble(tmX), Double.parseDouble(tmY));
+                stId = parsingXML.parsing("stationId", index);
+                stName = parsingXML.parsing("stationNm", index);
+                arsId = parsingXML.parsing("arsId", index);
             }
         } catch (ParserConfigurationException | InterruptedException e) {
             e.printStackTrace();
@@ -75,5 +89,49 @@ public class GetStationInfo {
         stInfo.add(arsId);
 
         return stInfo;
+    }
+
+    // 현재 위치와 가장 가까운 정류장 인덱스 리턴
+    private int findNearStation(ArrayList<ArrayList<Double>> list, double lon, double lat) {
+        int ret = 0;
+        double minDist = START_DISTANCE_FOR_GET_STATION;
+
+        for(int i=0; i<list.size(); i++) {
+            double dist = calDistance(list.get(i).get(1), list.get(i).get(0), lat, lon);
+            if (minDist >= dist) {
+                ret = i;
+                minDist = dist;
+            }
+        }
+
+        return ret;
+    }
+
+    // 좌표 사이에 거리 계산
+    // @return : m단위 거리
+    private double calDistance(double lat1, double lon1, double lat2, double lon2){
+
+        double theta, dist;
+        theta = lon1 - lon2;
+        dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2)) + Math.cos(deg2rad(lat1))
+                * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
+        dist = Math.acos(dist);
+        dist = rad2deg(dist);
+
+        dist = dist * 60 * 1.1515;
+        dist = dist * 1.609344;    // mile to km
+        dist = dist * 1000.0;      // km to m
+
+        return dist;
+    }
+
+    // degree to radian
+    private double deg2rad(double deg){
+        return (double)(deg * Math.PI / (double)180d);
+    }
+
+    // radian to degree
+    private double rad2deg(double rad){
+        return (double)(rad * (double)180d / Math.PI);
     }
 }
